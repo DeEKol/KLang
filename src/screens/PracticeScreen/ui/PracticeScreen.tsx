16261eb567a5677532f8dc318e6755cbeec06780
Aleksey Kondakov <112566801+A-Kimpo@users.noreply.github.com>
2024-09-12 03:16:40 +0500

    upd: move WordMatcher in practice screen

--- src/screens/PracticeScreen/ui/PracticeScreen.tsx
+++ src/screens/PracticeScreen/ui/PracticeScreen.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { Text, View } from "react-native";
+import { WordMatcher } from "features/WordMatcher";
 
 export const PracticeScreen = () => {
   const { t } = useTranslation("practiceScreen");
 
   return (
-    <View>
-      <Text>{t("This is practice screen")}</Text>
-    </View>
+    // <View>
+    //   <Text>{t("This is practice screen")}</Text>
+    // </View>
+    <WordMatcher />
   );
 };
